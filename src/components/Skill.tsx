6a6091811cfbdfba499cda7cd59fb373726c7334
tmraaex <tmr.aaex@gmail.com>
2025-05-24 04:54:20 +0200

    Added meta data and li tag fix

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,10 +2,10 @@ interface SkillProps {
     name: string;
 }
 
-export default function Skill({ name }: SkillProps) {
+export default function Skill({name}: SkillProps) {
     return (
-        <div className="inline-block rounded-full px-4 py-2 bg-white/10 text-white text-sm mr-2 mb-2">
+        <li className="inline-block rounded-full px-4 py-2 bg-white/10 text-white text-sm mr-2 mb-2">
             {name}
-        </div>
+        </li>
     );
 }
