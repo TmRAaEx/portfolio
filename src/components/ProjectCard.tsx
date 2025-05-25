8113e16fa3435a234da60c0f94921c79e85f6113
tmraaex <tmr.aaex@gmail.com>
2025-05-25 03:02:46 +0200

    changed link icon

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -31,33 +31,61 @@ export default function ProjectCard({project}: { project: Project }) {
                         <a
                             href={project.link}
                             target="_blank"
-                            className="text-blue-400 underline hover:text-blue-300 transition"
+                            rel="noopener noreferrer"
+                            className="inline-flex items-center gap-1 text-blue-400 underline hover:text-blue-300 transition"
                         >
                             Demo
+                            <img src="/icons/link.svg" width={16} height={16} alt="Extern länk"/>
                         </a>
+
                     )}
                     {project.npm_link && (
                         <a
                             href={project.npm_link}
                             target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative group inline-block"
                         >
                             <img
                                 src="/icons/npm.svg"
                                 alt="NPM"
-                                className="w-12 h-12 hover:opacity-80 transition"
+                                className="w-8 h-8 hover:opacity-80 transition"
                             />
+                            <span className="absolute top-1/2 left-full ml-2 -translate-y-1/2
+                     hidden group-hover:block
+                     bg-gray-800 text-white text-xs rounded py-1 px-2
+                     whitespace-nowrap
+                     pointer-events-none
+                     z-10">
+      Visa på npm
+    </span>
                         </a>
                     )}
-                    {
-                        project.git_link && (
-                            <a href={project.git_link} target="_blank">
-                                <img
-                                    src="/icons/github.svg"
-                                    alt="GitHub"
-                                    className="w-6 h-6 hover:opacity-80 transition"
-                                />
-                            </a>
-                        )}
+
+                    {project.git_link && (
+                        <a
+                            href={project.git_link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative group inline-block"
+                        >
+                            <img
+                                src="/icons/github.svg"
+                                alt="GitHub"
+                                className="w-6 h-6 hover:opacity-80 transition"
+                            />
+                            <span className="absolute top-1/2 left-full ml-2 -translate-y-1/2
+                     hidden group-hover:block
+                     bg-gray-800 text-white text-xs rounded py-1 px-2
+                     whitespace-nowrap
+                     pointer-events-none
+                     z-10">
+      Visa på GitHub
+    </span>
+                        </a>
+                    )}
+
+
                 </div>
             </div>
         </li>
